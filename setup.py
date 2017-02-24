6f3d14fca791b0f26d8e70d01ce7a8c022f1c2df
Rushan <vinder_2@mail.ru>
2017-02-24 21:30:19 +0300

    First init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='topser',
+      version='0.1',
+      description='The simple package',
+      url='http://github.com/vitakrya/topser',
+      author='Vita Kr',
+      author_email='vinder_2@mail.ru',
+      license='MIT',
+      packages=['topser'],
+      zip_safe=False)
